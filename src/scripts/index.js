e0643ac82608c0bc7d75d158d55f92559ab8c640
unknown <jackblack.nt@ya.ru>
2020-02-03 17:23:05 +1100

    New form params added

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,2 +1,7 @@
 import "../styles/index.css";
 
+const searchForm = document.forms.searchForm;
+const searchInpit = searchForm.elements[0];
+const searchSubmit = searchForm.elements[1];
+
+console.log(searchSubmit)
