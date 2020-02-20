a3425029a4938784cc9f898376f3cd457e6d5794
unknown <jackblack.nt@ya.ru>
2020-02-20 13:02:07 +1100

    Added feature: show the default image instead of the unavailable one

--- src/scripts/components/Card.js
+++ src/scripts/components/Card.js
@@ -1,10 +1,12 @@
+import "../../images/default-cardpic.jpg";
+
 export default class Card {
     constructor(func) {
         this._dateFormater = func;
         this._cardBlock = document.createElement('li');
         this._cardTemplate = `
         <a class="card" target="_blank">
-            <img class="card__pic" alt="news illustration">
+            <img class="card__pic" alt="news illustration"></img>
             <time class="card__date"></time>
             <h4 class="card__title"></h4>
             <p class="card__text"></p>
@@ -17,15 +19,24 @@ export default class Card {
 
         const dateFormater = this._dateFormater();
         const dateObj = dateFormater.getFormatedDateObject(data.publishedAt);
-
         this._cardBlock.innerHTML = this._cardTemplate;
+        
+        /* change pic if it error on load */
+        this._cardBlock.querySelector('.card__pic').addEventListener('error', (event) => {
+            event.target.setAttribute('src', "../../images/default-cardpic.jpg");
+        });
+        
+        /* this._cardBlock.querySelector('.card__pic').onerror = () => {
+            this._cardBlock.querySelector('.card__pic').setAttribute('src', "../../images/default-cardpic.jpg");
+        } */
+        
         this._cardBlock.querySelector('.card').setAttribute('href', data.url);
         this._cardBlock.querySelector('.card__pic').setAttribute('src', data.urlToImage);
-        this._cardBlock.querySelector('.card__date').setAttribute('datetime', data.publishedAt)
+        this._cardBlock.querySelector('.card__date').setAttribute('datetime', data.publishedAt);
         this._cardBlock.querySelector('.card__date').innerText = dateObj.formatDate + ' ' + dateObj.formatMonthWord + ' ' + dateObj.formatYear;
         this._cardBlock.querySelector('.card__title').innerText = data.title;
         this._cardBlock.querySelector('.card__text').innerText = data.description;
-        this._cardBlock.querySelector('.card__link').innerText = data.source.name;        
+        this._cardBlock.querySelector('.card__link').innerText = data.source.name;
 
         return this._cardBlock;
     }
